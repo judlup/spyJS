2c9514387382d6f19548f23b99a66333ce0cfe61
JudLup Luna <judlup@gmail.com>
2018-01-16 01:12:25 -0500

    spyfy

--- example/index.js
+++ example/index.js
@@ -8,7 +8,6 @@ spyfy.watch_http('http://api.woonked.com/wuser/domoteco');
 //Twitter API credentials (consumer_key,consumer_secret,access_token_key,access_token_secret)  
 tw = spyfy.twitter('YOUR_CONSUMER_KEY','YOUR_CONSUMER_SECRET','YOUR_ACCESS_KEY','YOUR_TOKEN_SECRET')
 
-
 //TT Twitter
 spyfy.hashtalker(tw,'iot')
 
